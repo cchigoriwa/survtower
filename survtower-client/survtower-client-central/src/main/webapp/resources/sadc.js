78443160afb1cbd8ff70369734f1c5b870a67863
tdhlakama <tdhlakama@gmail.com>
2014-09-16 16:37:10 +0200

    -------------------------------------Sadc-----------------------------------------
    Login Page Created and Modified
    
    --------------------------------------------------------------------------------------

--- survtower-client/survtower-client-central/src/main/webapp/resources/sadc.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $('input[type="text"]').focus(function() {
-        $(this).addClass("focus");
-    });
- 
-    $('input[type="text"]').blur(function() {
-        $(this).removeClass("focus");
-    });
-});
