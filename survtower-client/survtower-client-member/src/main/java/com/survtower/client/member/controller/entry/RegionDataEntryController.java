b7b90c49cb22145750244d42e19b72c07407a773
tdhlakama <tdhlakama@gmail.com>
2015-05-20 13:32:09 +0200

    ==================SADC==========================
    Excel Data Entry Form Created
    
    ===================================== ==========

--- survtower-client/survtower-client-member/src/main/java/com/survtower/client/member/controller/entry/RegionDataEntryController.java
+++ survtower-client/survtower-client-member/src/main/java/com/survtower/client/member/controller/entry/RegionDataEntryController.java
@@ -9,7 +9,6 @@ import com.survtower.business.common.domain.Period;
 import com.survtower.business.common.domain.Program;
 import com.survtower.business.common.domain.Surveillance;
 import com.survtower.business.common.domain.SurveillanceData;
-import com.survtower.business.common.service.IndicatorService;
 import com.survtower.business.common.service.MemberService;
 import com.survtower.business.common.service.PeriodService;
 import com.survtower.business.common.service.ProgramService;
@@ -25,6 +24,7 @@ import com.survtower.business.member.service.RegionSurveillanceDataService;
 import com.survtower.client.member.utility.MessageInfor;
 import static com.survtower.client.member.utility.MessageInfor.errorMessages;
 import static com.survtower.client.member.utility.MessageInfor.inforMessages;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
@@ -35,6 +35,15 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.CellRangeAddress;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.primefaces.event.FileUploadEvent;
 
 /**
  *
@@ -150,17 +159,26 @@ public class RegionDataEntryController extends MessageInfor implements Serializa
         return surveillance;
     }
 
+    public void saveInitalDataValues() {
+        for (RegionSurveillanceData data : getSurveillanceDataList()) {
+            regionSurveillanceDataService.save(data);
+        }
+        if (getSurveillanceAudit().getId() == null) {
+            getSurveillanceAudit().setPeriod(period);
+            getSurveillanceAudit().setProgram(program);
+            getSurveillanceAudit().setRegion(region);
+            getSurveillanceAudit().setUploadedBy(getCurrentUser());
+            surveillanceAuditService.save(surveillanceAudit);
+        }
+    }
+
     public void setSurveillance(Surveillance surveillance) {
         this.surveillance = surveillance;
     }
 
     public String submitSurviellanceForm() {
-        try {
-            submitted = Boolean.TRUE;
-        } catch (Exception ex) {
-            submitted = Boolean.FALSE;
-            errorMessages("Surveillance Data Not Processed Succefully");
-        }
+        saveInitalDataValues();
+        submitted = Boolean.TRUE;
         return null;
     }
 
@@ -192,20 +210,10 @@ public class RegionDataEntryController extends MessageInfor implements Serializa
                 }
             }
 
-            if (getSurveillanceAudit().getId() == null) {
-                getSurveillanceAudit().setPeriod(period);
-                getSurveillanceAudit().setProgram(program);
-                getSurveillanceAudit().setUploadedBy(getCurrentUser());
-                getSurveillanceAudit().setUploadedOn(new Date());
-                getSurveillanceAudit().setRegion(region);
-            } else {
-                getSurveillanceAudit().setUploadedOn(new Date());
-            }
-            if (getSurveillanceAudit().getUploadedBy() == null) {
-                errorMessages("Audit Trail - Not Working");
-                return null;
-            }
+            getSurveillanceAudit().setUploadedBy(getCurrentUser());
+            getSurveillanceAudit().setUploadedOn(new Date());
             surveillanceAuditService.save(surveillanceAudit);
+
             inforMessages("Surviellance Data Saved Successfully");
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -297,6 +305,185 @@ public class RegionDataEntryController extends MessageInfor implements Serializa
                 return null;
             }
         }
+    }
+
+    public String downloadSurviellanceForm() {
+
+        saveInitalDataValues();
+
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+
+        HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+
+        response.setContentType("application/vnd.ms-excel");
+
+        response.setContentType("application/force-download");
+
+        String downloadFile = getRegion().getName() + " " + getPeriod().getName() + ".xls";
+
+        downloadFile = downloadFile.replace(" ", "_");
+
+        response.addHeader("content-disposition", "inline;  filename=" + downloadFile + "");
+
+        try {
+            Workbook workbook = writeFile(new HSSFWorkbook());
+            workbook.write(response.getOutputStream());
+            inforMessages("Data Entry Form Created");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            errorMessages("Error Creating Data Entry Form");
+        }
+        facesContext.responseComplete();
+        return null;
+    }
+
+    public Workbook writeFile(Workbook workbook) {
+        workbook = new HSSFWorkbook();
+
+        Sheet sheet = workbook.createSheet(getRegion().getName() + " " + getPeriod().getName() + " " + getProgram().getName());
+
+        CellStyle style = workbook.createCellStyle();
+        Font font = workbook.createFont();
+        font.setBoldweight(Font.BOLDWEIGHT_BOLD);
+        font.setFontHeightInPoints((short) 13);
+        style.setFont(font);
+        style.setWrapText(true);
+
+        int rowIndex = 0;
+        sheet.createRow(rowIndex);
+        sheet.getRow(rowIndex).setZeroHeight(true);
+        sheet.setColumnHidden(rowIndex, true);
+        sheet.getRow(rowIndex).createCell(1).setCellValue(getRegion().getUuid());
+        sheet.getRow(rowIndex).createCell(2).setCellValue(getProgram().getUuid());
+        sheet.getRow(rowIndex).createCell(3).setCellValue(getPeriod().getUuid());
+        rowIndex++;
+        
+        sheet.createRow(rowIndex);
+        sheet.getRow(rowIndex).createCell(1).setCellValue("Regional Surveillance Data Entry Form");
+        sheet.getRow(rowIndex).createCell(2).setCellValue("");
+        sheet.getRow(rowIndex).getCell(1).setCellStyle(style);
+        sheet.getRow(rowIndex).setHeight((short) 400);        
+        sheet.addMergedRegion(new CellRangeAddress(
+            rowIndex, //first row (0-based)
+            rowIndex, //last row  (0-based)
+            1, //first column (0-based)
+            2  //last column  (0-based)
+    ));
+
+        rowIndex++;
+
+        sheet.createRow(rowIndex);
+        sheet.getRow(rowIndex).createCell(1).setCellValue(getRegion().getName());
+        sheet.getRow(rowIndex).createCell(2).setCellValue(getProgram().getName());
+        sheet.getRow(rowIndex).createCell(3).setCellValue(getPeriod().getName());
+        sheet.getRow(rowIndex).getCell(1).setCellStyle(style);
+        sheet.getRow(rowIndex).setHeight((short) 400);
+
+        for (int i = 1; i < 4; i++) {
+            sheet.getRow(rowIndex).getCell(i).setCellStyle(style);
+        }
+
+        rowIndex++;
+
+        sheet.createRow(rowIndex);
+        sheet.getRow(rowIndex).createCell(1).setCellValue("Indicator Name");
+        sheet.getRow(rowIndex).createCell(2).setCellValue("Numerator Value");
+        sheet.getRow(rowIndex).createCell(3).setCellValue("Denominator Value");
+        sheet.getRow(rowIndex).setHeight((short) 400);
+        for (int i = 1; i < 4; i++) {
+            sheet.getRow(rowIndex).getCell(i).setCellStyle(style);
+        }
+        rowIndex++;
+
+        for (RegionSurveillanceData data : surveillanceDataList) {
+            sheet.createRow(rowIndex);
+            sheet.getRow(rowIndex).createCell(0).setCellValue(data.getUuid());
+            sheet.getRow(rowIndex).createCell(1).setCellValue(data.getSurveillanceData().getIndicator().getName());
+            font.setFontHeightInPoints((short) 12);
+            sheet.getRow(rowIndex).getCell(1).setCellStyle(style);
+            sheet.getRow(rowIndex).createCell(2).setCellType(Cell.CELL_TYPE_NUMERIC);
+            sheet.getRow(rowIndex).createCell(3).setCellType(Cell.CELL_TYPE_NUMERIC);
+            sheet.getRow(rowIndex).setHeight((short) 400);
+            rowIndex++;
+        }
+
+        sheet.autoSizeColumn(1);
+        sheet.autoSizeColumn(2);
+        sheet.autoSizeColumn(3);
+
+        return workbook;
+
+    }
+
+    public String handleFileUpload(FileUploadEvent event) {
+
+        Workbook workbook = null;
+        try {
+            workbook = new HSSFWorkbook(event.getFile().getInputstream());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            errorMessages("Could Not Locate Key Identifiers for Excel Upload, File Cannot be used");
+            return null;
+        }
+
+        Sheet sheet = workbook.getSheetAt(0);
+        String regionId, programId, periodId;
+        try {
+            regionId = sheet.getRow(0).getCell(0).getStringCellValue();
+            programId = sheet.getRow(0).getCell(1).getStringCellValue();
+            periodId = sheet.getRow(0).getCell(2).getStringCellValue();
+        } catch (Exception e) {
+            errorMessages("Could Not Locate Key Identifiers for Excel Upload, File Cannot be used");
+            return null;
+        }
+
+        program = programService.findByUuid(programId);
+        period = periodService.findByUuid(periodId);
+        region = regionService.findByUuid(regionId);
+
+        if (program == null) {
+            errorMessages("Program Surviellance not found, File Cannot be used");
+            return null;
+        }
+
+        if (period == null) {
+            errorMessages("Period Surviellance not found, File Cannot be used");
+            return null;
+        }
+
+        if (region == null) {
+            errorMessages("Region not found, File Cannot be used");
+            return null;
+        }
+
+        surveillance = surveillanceService.get(program, period, memberService.getCurrentMember());
+
+        if (surveillance == null) {
+            errorMessages("Surviellance Reporting not Found, File Cannot be used");
+            return null;
+        }
+
+        int errors = 0;
+        int itemsSaved = 0;
+        for (int rowIndex = 3; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+            RegionSurveillanceData data = regionSurveillanceDataService.findByUuid(sheet.getRow(rowIndex).getCell(0).getStringCellValue());
+            if (data != null && data.getRegion().getId().equals(region.getId())) {
+                try {
+                    data.setNumeratorValue(sheet.getRow(rowIndex).getCell(2).getNumericCellValue());
+                    data.setNumeratorValue(sheet.getRow(rowIndex).getCell(3).getNumericCellValue());
+                    regionSurveillanceDataService.save(data);
+                    itemsSaved++;
+                } catch (Exception e) {
+                    errors++;
+                }
+            }
+            rowIndex++;
+        }
+
+        inforMessages(itemsSaved + "Entries Uploaded. " + errors + " Entries Failed to Upload.");
+        surveillanceDataList.addAll(regionSurveillanceDataService.findAll(surveillance, region));
+        return null;
+
     }
 
 }
