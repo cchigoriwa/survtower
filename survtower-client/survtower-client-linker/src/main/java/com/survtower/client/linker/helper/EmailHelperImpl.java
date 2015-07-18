19127e140fe779748949c496d77ebce423ea3f11
Daniel Nkhoma <danielnkhoma@gmail.com>
2015-07-18 15:20:16 +0200

    WebUtility linker adding base url

--- survtower-client/survtower-client-linker/src/main/java/com/survtower/client/linker/helper/EmailHelperImpl.java
+++ survtower-client/survtower-client-linker/src/main/java/com/survtower/client/linker/helper/EmailHelperImpl.java
@@ -4,9 +4,6 @@ import com.survtower.business.central.domain.ResetCentralUserPasswordRequest;
 import com.survtower.business.central.domain.ResetMemberSecurityPasswordRequest;
 import com.survtower.business.central.service.EmailHelper;
 import com.survtower.client.linker.util.WebUtilityImpl;
-import java.net.MalformedURLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.springframework.stereotype.Component;
 
 /**
@@ -31,17 +28,12 @@ public class EmailHelperImpl implements EmailHelper {
         StringBuilder sb = new StringBuilder();
         sb.append("Changing your password is simple. Please use the link below");
         sb.append(" within 24 hours.  ");
-        String webUrl = null;
-        try {
-            webUrl = WebUtilityImpl.getBasePath();
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(EmailHelperImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-//        String webUrl = "http://localhost:8080";
+
+        String webUrl = WebUtilityImpl.getBasePath();
         sb.append(webUrl);
-        sb.append("/survtower-client-linker/reset-password/");
+        sb.append("/reset-password/");
         sb.append(resetPasswordRequest.getFirstTag());
-        sb.append("&tok=");
+        sb.append("?tok=");
         sb.append(resetPasswordRequest.getSecondTag());
         return sb.toString();
     }
--- survtower-client/survtower-client-linker/src/main/java/com/survtower/client/linker/util/WebUtilityImpl.java
+++ survtower-client/survtower-client-linker/src/main/java/com/survtower/client/linker/util/WebUtilityImpl.java
@@ -1,9 +1,8 @@
 package com.survtower.client.linker.util;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import javax.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  *
@@ -11,24 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class WebUtilityImpl {
 
-    @Autowired
     private static HttpServletRequest request;
 
-    public static String getBasePath() throws MalformedURLException {
-
+    public static String getBasePath() {
+        request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
         return getUrlBase(request);
     }
 
-    public static String getUrlBase(HttpServletRequest request)
-            throws MalformedURLException {
-        URL requestUrl = new URL(request.getRequestURL().toString());
-        String portString
-                = requestUrl.getPort() == -1 ? "" : ":" + requestUrl.getPort();
-        return requestUrl.getProtocol()
-                + "://" + requestUrl.getHost() + portString + "/";
-    }
+    public static String getUrlBase(HttpServletRequest request) {
 
-    public static void main(String[] args) throws MalformedURLException {
-        System.out.println(getBasePath());;
+        String url = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+        return url;
     }
 }
