d54c43ae8f82bf38492d16f95d48a0a01a429d58
Daniel Nkhoma <danielnkhoma@gmail.com>
2015-07-20 15:33:22 +0200

    modifying indicator list page

--- survtower-business/survtower-business-common/src/main/java/com/survtower/business/common/domain/Period.java
+++ survtower-business/survtower-business-common/src/main/java/com/survtower/business/common/domain/Period.java
@@ -12,7 +12,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
-import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
